b75fda727bf3962f8104d2043d2ee8822a0a10ee
SaYamauchi <109891780+SaYamauchi@users.noreply.github.com>
2022-11-12 10:58:08 +0500

    Add files via upload

---
+++ src/items/item.interface.ts
@@ -0,0 +1,13 @@
+export interface BaseItem {
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+  }
+  
+  export interface Item extends BaseItem {
+    id: number;
+  }
+
+
+  
